6e9205cd5a4de237e8ae3242abe9134446622473
Nicole Pimenta <niipimenta@hotmail.com>
2020-12-29 20:42:54 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+		<title>Meu primeiro site em PHP! WOOHOO! </title> 
+	</head> 
+	
+	<body> 
+	
+	  <?php 
+			for ( $i = 0 ; $i <10 ; $i++ ) {
+				print ( "Linha número " .$i. "<br />");
+			}  
+		?>
+		
+		</body> 
+		
+		</html>
+
